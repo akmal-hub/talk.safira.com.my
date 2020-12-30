2d13f8007ccf05a649139eff8afc3eeb0799de52
akmal-hub <74760392+akmal-hub@users.noreply.github.com>
2020-12-30 08:41:20 +0800

    Create index.php

---
+++ index.php
@@ -0,0 +1,163 @@
+<!DOCTYPE html>
+
+<html>
+
+<head>
+
+<title>Touched By ./A-G-X</title>
+
+<link rel="preconnect" href="https://fonts.gstatic.com">
+
+<link href="https://fonts.googleapis.com/css2?family=Permanent+Marker&display=swap" rel="stylesheet">
+
+<link rel="preconnect" href="https://fonts.gstatic.com">
+
+<link href="https://fonts.googleapis.com/css2?family=Audiowide&display=swap" rel="stylesheet">
+
+<meta name="Abstract" content="FUCK YOU MALAYSIA" >
+
+<meta name="description" content="FUCK YOU MALAYSIA" >
+
+<style type="text/css">
+
+body,html{
+
+  text-align:center;
+
+  background:black;
+
+  top:50%;
+
+  }
+
+.b{
+
+  font-family: 'Audiowide', cursive;
+
+  color:white;
+
+  text-shadow:8px 0px 0px red;
+
+}
+
+.a{
+
+  font-family: 'Audiowide', cursive;
+
+  color:white;
+
+}
+
+.c{
+
+    font-family: 'Audiowide', cursive;
+
+  color:red;
+
+  text-shadow:0px 0px 4px white;
+
+  }
+
+  
+
+.d{
+
+  font-family: 'Audiowide', cursive;
+
+  color:white;
+
+  text-shadow:0px 0px 4px red;
+
+}
+
+.e{
+
+  font-family: 'Permanent Marker', cursive;
+
+  color:white;
+
+}
+
+button{
+
+  border:solid 4px red;
+
+  border-radius:14px;
+
+  width:180px;
+
+  background:black;
+
+  color:white;
+
+  font-family: 'Permanent Marker', cursive;
+
+}
+
+</style>
+
+</head>
+
+<body>
+
+<center>
+
+<br><br><br><br><br>
+
+<b><font class="b"  size="9" >Touched By ./A-G-X</font></b></center>
+
+<img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSwilmfgq5ugbuek2nvq0UKDe6xD-1zdbmQXA&usqp=CAU" width="900" height="600"  ><br><br>
+
+<br><br><hr color="white">
+
+<font class="c" size="9">FUCK YOU MALAYSIA!</font><br>
+
+<font class="d" size="9" >MERDEKA INDONESIA!</font>
+
+<hr color="white"><br><br><br><br><br>
+
+<font class="a" size="5">Indonesia Hackers Rulez</font><br><br><br><br><br>
+
+  <font class="e" size="4">Greatz To<br>
+
+-=[<marquee scrollamount="3" width="600"><font>All My friends || Wolf Cyber Team || Konoha Xploit || Blackhat Tersakiti || All defacer indonesia </marquee>]=-</font>
+
+<br>
+
+  <audio id="Audio">
+
+    <source src="https://e.top4top.io/m_1824fh53u0.mp3" type="audio/mpeg">
+
+    <source src="https://e.top4top.io/m_1824fh53u0.mp3" type="audio/ogg">
+
+  </audio>
+
+  <button onclick="playAudio()" type="button">Play</button>
+
+  <button onclick="pauseAudio()" type="button">Pause</button>
+
+<script>
+
+  var x = document.getElementById('Audio');
+
+  function playAudio(){
+
+    x.play();
+
+  }
+
+  var x = document.getElementById('Audio');
+
+  function pauseAudio(){
+
+    x.pause();
+
+  }
+
+</script>
+
+</center>
+
+</body>
+
+</html>
